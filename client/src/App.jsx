fea36569819c1479994d30e8bffc1b33888a0039
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-10 22:37:51 +0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 import toast from "react-hot-toast";
+import { useEffect, useRef } from "react";
+import Pikaday from "pikaday";
 
 const App = () => {
+  const myDatepicker = useRef(null);
+  useEffect(() => {
+    const picker = new Pikaday({
+      field: myDatepicker.current,
+    });
+    return () => picker.destroy();
+  }, []);
   const showToast = () => {
     toast.success("This is a success toast!");
-  }
+  };
+
   return (
     <div className="flex flex-col justify-center items-center h-screen w-screen bg-gray-300">
       <h1 className="text-3xl text-red-500 underline font-bold">
         Welcome to the Chat App
       </h1>
       <div className="my-2 flex gap-3">
-        <button onClick={showToast} className="btn btn-neutral">Neutral</button>
+        <button onClick={showToast} className="btn btn-neutral">
+          Neutral
+        </button>
         <button className="btn btn-primary">Primary</button>
         <button className="btn btn-secondary">Secondary</button>
         <button className="btn btn-accent">Accent</button>
@@ -20,6 +32,12 @@ const App = () => {
         <button className="btn btn-warning">Warning</button>
         <button className="btn btn-error">Error</button>
       </div>
+      <input
+        type="text"
+        className="input pika-single"
+        defaultValue="Pick a date"
+        ref={myDatepicker}
+      />
     </div>
   );
 };
