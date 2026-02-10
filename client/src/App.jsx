8bfb325abc0f9b63de03d3790ba89950d7fc3fd6
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-10 22:18:28 +0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import toast from "react-hot-toast";
 
 const App = () => {
+  const showToast = () => {
+    toast.success("This is a success toast!");
+  }
   return (
     <div className="flex flex-col justify-center items-center h-screen w-screen bg-gray-300">
       <h1 className="text-3xl text-red-500 underline font-bold">
         Welcome to the Chat App
       </h1>
       <div className="my-2 flex gap-3">
-        <button className="btn btn-neutral">Neutral</button>
+        <button onClick={showToast} className="btn btn-neutral">Neutral</button>
         <button className="btn btn-primary">Primary</button>
         <button className="btn btn-secondary">Secondary</button>
         <button className="btn btn-accent">Accent</button>
