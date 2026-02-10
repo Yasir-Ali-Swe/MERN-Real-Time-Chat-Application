1c7ae03788e530ccc6f913c8fbe28eb81304bff7
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-10 21:46:28 +0500

    Add server entry point

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.connect.js";
+import { PORT } from "./config/env.js";
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  }),
+);
+
+app.listen(PORT, async () => {
+  console.log("Server is running on port 5000");
+  await connectDB();
+});
