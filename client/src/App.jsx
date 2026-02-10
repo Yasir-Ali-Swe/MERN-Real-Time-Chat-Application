c89d66be47b2d085aad18cc1e437d3acbe5a713c
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-10 22:09:55 +0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div className='flex justify-center items-center h-screen w-screen bg-gray-300'>
-      <h1 className='text-3xl text-red-500 underline font-bold'>Welcome to the Chat App</h1>
+    <div className="flex flex-col justify-center items-center h-screen w-screen bg-gray-300">
+      <h1 className="text-3xl text-red-500 underline font-bold">
+        Welcome to the Chat App
+      </h1>
+      <div className="my-2 flex gap-3">
+        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+        <button className="btn btn-info">Info</button>
+        <button className="btn btn-success">Success</button>
+        <button className="btn btn-warning">Warning</button>
+        <button className="btn btn-error">Error</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
