92276150ab46da76628a818820353256ada1a703
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-20 20:05:31 +0500

    Update email verification utility

--- server/utils/verify.email.js
+++ server/utils/verify.email.js
@@ -1,12 +1,15 @@
 import nodemailer from "nodemailer";
 import { EMAIL, EMAIL_PASSWORD } from "../config/env.js";
 import { verifyEmailTemplate } from "../emails/verify.email.template.js";
+import { forgotPasswordTemplate } from "../emails/reset.password.email.template.js";
 
 export const sendVerificationEmail = async ({
   name,
   email,
   token,
   expireIn,
+  subject,
+  purpose, //if purpose is 1 then user verifyEmailTemplate otherwise user forgetPasswordTemplate
 }) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
@@ -15,12 +18,18 @@ export const sendVerificationEmail = async ({
       pass: EMAIL_PASSWORD,
     },
   });
-
+  sendVerificationEmail;
+  let htmlContent;
+  if (purpose === 1) {
+    htmlContent = verifyEmailTemplate({ name, token, expireIn });
+  } else {
+    htmlContent = forgotPasswordTemplate({ name, token, expireIn });
+  }
   const mailOptions = {
     from: EMAIL,
     to: email,
-    subject: "Verify Your Email Address",
-    html: verifyEmailTemplate({ name, token, expireIn }),
+    subject,
+    html: htmlContent,
   };
 
   try {
