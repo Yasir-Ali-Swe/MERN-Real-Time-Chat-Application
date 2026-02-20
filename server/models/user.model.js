1a3ad366af42ba5cbee26dab11f2057abbfa98bc
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-20 14:10:50 +0500

    User Model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -23,6 +23,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "",
     },
+    tokenVersion: {
+      type: Number,
+      default: 0,
+    },
   },
   { timestamps: true },
 );
