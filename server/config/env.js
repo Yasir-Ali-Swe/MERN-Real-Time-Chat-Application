8ebe07d8a31cb1624e46b3de40a34aed243e4806
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-20 15:02:23 +0500

    Update server environment config

--- server/config/env.js
+++ server/config/env.js
@@ -5,5 +5,6 @@ const MONGO_URI = process.env.MONGO_URI;
 const JWT_SECRET = process.env.JWT_SECRET_KEY;
 const EMAIL = process.env.EMAIL;
 const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
+const CLIENT_URL = process.env.CLIENT_URL;
 
-export { PORT, MONGO_URI, JWT_SECRET, EMAIL, EMAIL_PASSWORD };
+export { PORT, MONGO_URI, JWT_SECRET, EMAIL, EMAIL_PASSWORD, CLIENT_URL };
