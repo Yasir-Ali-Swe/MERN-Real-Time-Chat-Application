33e272d911568eeaa599663ae61d113e2844b030
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-20 19:04:13 +0500

    Update auth routes

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { register } from "../controllers/auth.controller.js";
+import { register, verifyEmail } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/verify-email/:token", verifyEmail);
 
 export default router;
-
