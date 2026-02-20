03246443e901cedc19bc7916b2d7310a98877026
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-20 14:16:47 +0500

    env.js

--- server/config/env.js
+++ server/config/env.js
@@ -1,7 +1,9 @@
 import "dotenv/config.js";
 
-const PORT = process.env.PORT
-const MONGO_URI = process.env.MONGO_URI
-const JWT_SECRET = process.env.JWT_SECRET_KEY
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
+const JWT_SECRET = process.env.JWT_SECRET_KEY;
+const EMAIL = process.env.EMAIL;
+const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
 
-export { PORT, MONGO_URI, JWT_SECRET }
+export { PORT, MONGO_URI, JWT_SECRET, EMAIL, EMAIL_PASSWORD };
