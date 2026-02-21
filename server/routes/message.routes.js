9043fbaac450d514fe1cf8501b27fa455844e6d6
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-21 21:19:06 +0500

    Add message routes

--- server/routes/message.routes.js
+++ server/routes/message.routes.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { sendMessage } from "../controllers/message.controller.js";
+import {
+  sendMessage,
+  getConversation,
+} from "../controllers/message.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/send-message", authMiddleware, sendMessage);
-
+router.get(
+  "/get-conversation/:conversationId",
+  authMiddleware,
+  getConversation,
+);
 export default router;
