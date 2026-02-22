c06bd4d7204f9a23c638ee343c594e63a46d2712
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-22 13:18:56 +0500

    Update login page

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,11 +1,108 @@
-import React from 'react'
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "../components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { MessageCircleMore, Eye, EyeOff } from "lucide-react";
 
 const login = () => {
-  return (
-    <div className='flex justify-center items-center h-screen'>
-        <h1 className='text-3xl text-primary font-black underline'>Login Page</h1>
-    </div>
-  )
-}
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-export default login
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen w-screen px-3 lg:p-0">
+      <div className="my-5 flex items-center justify-center gap-3">
+        <h1 className="text-3xl font-bold">NextTalk</h1>
+        <MessageCircleMore className="size-9" />
+      </div>
+      <Card className={"w-full max-w-xs md:max-w-sm lg:max-w-md rounded-sm"}>
+        <CardHeader>
+          <CardTitle className={"text-xl"}>Login</CardTitle>
+          <CardDescription>Enter your credentials to login</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit}>
+            <div className="space-y-1 mb-3">
+              <Label htmlFor="email" className={"text-md"}>
+                Email
+              </Label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="Enter your email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="space-y-1 my-3 relative">
+              <Label htmlFor="password" className={"text-md"}>
+                Password
+              </Label>
+              <Input
+                id="password"
+                name="password"
+                type={showPassword ? "text" : "password"}
+                placeholder="Enter your password"
+                value={formData.password}
+                onChange={handleChange}
+              />
+              <div className="absolute right-3 top-9">
+                {showPassword ? (
+                  <EyeOff
+                    className="size-5 cursor-pointer"
+                    onClick={togglePasswordVisibility}
+                  />
+                ) : (
+                  <Eye
+                    className="size-5 cursor-pointer"
+                    onClick={togglePasswordVisibility}
+                  />
+                )}
+              </div>
+            </div>
+            <Button type="submit" className={"w-full my-3"}>
+              Login
+            </Button>
+          </form>
+        </CardContent>
+        <CardFooter className={"flex justify-center items-center"}>
+          <h1 className="text-muted-foreground">
+            Don't have an account?{" "}
+            <Link to="/auth/register" className="text-primary hover:underline">
+              Register
+            </Link>
+          </h1>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default login;
