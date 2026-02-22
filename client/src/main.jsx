b65feaffaba6d498280b2b2f8671985b48cbe997
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-22 15:13:48 +0500

    Update main entry file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,14 @@ import "./index.css";
 import App from "./App.jsx";
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router-dom";
+import Store from "@/store/store";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-    <Toaster position="top-center" reverseOrder={false} />
-  </BrowserRouter>,
+  <Provider store={Store}>
+    <BrowserRouter>
+      <App />
+      <Toaster position="top-center" reverseOrder={false} />
+    </BrowserRouter>
+  </Provider>,
 );
