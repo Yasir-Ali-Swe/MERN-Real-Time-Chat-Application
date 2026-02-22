c5b5ca320c4a71264a7d6983bd7a90242e1bdfbb
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-22 12:43:01 +0500

    Add chat page

---
+++ client/src/pages/chat.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Chat = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl text-primary font-black underline">Chat Page</h1>
+    </div>
+  );
+};
+
+export default Chat;
