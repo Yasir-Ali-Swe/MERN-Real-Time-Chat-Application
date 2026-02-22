b13e765ca98608de39b54c41a0d971439ee8670d
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-22 16:47:57 +0500

    Add routes folder

---
+++ client/src/routes/protected-routes.jsx
@@ -0,0 +1,23 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Loader } from "lucide-react";
+
+const ProtectedRoutes = () => {
+  const { isAuthenticated, isLoading } = useSelector((state) => state.auth);
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-8 font-semibold animate-spin" />
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/auth/login" replace />;
+  }
+
+  return <Outlet />;
+};
+
+export default ProtectedRoutes;
