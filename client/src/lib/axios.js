1cf3fc2a2e6d2e46ce1a18d5216871aceec3fe8e
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-22 18:49:37 +0500

    Update axios helper

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import Store from "@/store/store";
-import { setAccessToken, logout } from "../features/auth/authSlice";
+import { setAccessToken, logout } from "../features/auth/auth-slice";
 
 const API_BASE_URL = "http://localhost:5000/api";
 
