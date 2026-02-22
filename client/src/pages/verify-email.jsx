4c74ac930b2078a25783dce959245e19ec14554e
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-22 18:49:51 +0500

    Update verify email page

--- client/src/pages/verify-email.jsx
+++ client/src/pages/verify-email.jsx
@@ -1,13 +1,37 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { Loader, MessageCircleMore, MailCheck } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { toast } from "react-hot-toast";
+import { verifyEmail } from "../lib/auth-api";
+import { useMutation } from "@tanstack/react-query";
 
 const VerifyEmail = () => {
   const { token } = useParams();
   const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
+
+  const { mutate } = useMutation({
+    mutationFn: verifyEmail,
+    onSuccess: (response) => {
+      console.log("Email verification successful:", response);
+      toast.success(response.message || "Email verified successfully!");
+      setLoading(false);
+    },
+    onError: (error) => {
+      console.error("Email verification failed:", error);
+      toast.error(
+        error.response?.data?.message || "Email verification failed!",
+      );
+      setLoading(false);
+    },
+  });
+
+  useEffect(() => {
+    if (token) {
+      mutate(token);
+    }
+  }, [token, mutate]);
 
   return (
     <div className="flex flex-col justify-center items-center h-screen w-screen px-3 lg:p-0">
@@ -15,7 +39,15 @@ const VerifyEmail = () => {
         <h1 className="text-3xl font-bold">NexTalk</h1>
         <MessageCircleMore className="size-9" />
       </div>
+
       {loading ? (
+        <div className="flex items-center gap-4">
+          <h1 className="text-2xl font-semibold">
+            Please wait, we are verifying your request
+          </h1>
+          <Loader className="size-8 font-semibold animate-spin" />
+        </div>
+      ) : (
         <>
           <div className="flex items-center gap-4">
             <h1 className="text-2xl font-semibold">
@@ -23,23 +55,9 @@ const VerifyEmail = () => {
             </h1>
             <MailCheck className="size-8 font-semibold text-green-500" />
           </div>
-          <Button
-            className="mt-5"
-            onClick={() => {
-              navigate("/auth/login");
-            }}
-          >
+          <Button className="mt-5" onClick={() => navigate("/auth/login")}>
             Go to Login
           </Button>
-        </>
-      ) : (
-        <>
-          <div className="flex items-center gap-4">
-            <h1 className="text-2xl font-semibold">
-              Please wait we are verifying your request
-            </h1>
-            <Loader className="size-8 font-semibold animate-spin" />
-          </div>
         </>
       )}
     </div>
