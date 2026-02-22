ab90cebc4396a6d5918cf526b5b001749dab7903
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-22 12:43:09 +0500

    Add register page

---
+++ client/src/pages/register.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const register = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl text-primary font-black underline">
+        Register Page
+      </h1>
+    </div>
+  );
+};
+
+export default register;
