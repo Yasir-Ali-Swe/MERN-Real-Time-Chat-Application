4077594c4f888f5d1f53bb1bc1e04dca7e3d7e10
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-22 12:43:06 +0500

    Add login page

---
+++ client/src/pages/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+        <h1 className='text-3xl text-primary font-black underline'>Login Page</h1>
+    </div>
+  )
+}
+
+export default login
