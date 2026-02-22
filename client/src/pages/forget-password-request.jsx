ad7a29f770523e5959eed303067c5a01e2147fe0
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-22 12:43:04 +0500

    Add forget password request page

---
+++ client/src/pages/forget-password-request.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const forgetPasswordRequest = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl text-primary font-black underline">
+        Forget Password Request Page
+      </h1>
+    </div>
+  );
+};
+
+export default forgetPasswordRequest;
