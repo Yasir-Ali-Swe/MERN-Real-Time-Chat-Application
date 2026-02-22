9256a4db62e0cb6a38ce2a5d8b329e9b109ec8b8
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-22 15:19:06 +0500

    Update main entry file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById("root")).render(
   <Provider store={Store}>
     <BrowserRouter>
       <App />
-      <Toaster position="top-center" reverseOrder={false} />
+      <Toaster position="top-right" reverseOrder={false} />
     </BrowserRouter>
   </Provider>,
 );
