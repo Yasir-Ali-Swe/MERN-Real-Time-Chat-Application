53eab74ae7127dc4c5c980d4569c7c5f2607d4a8
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-24 01:45:22 +0500

    Update chat layout component

--- client/src/components/chat/chat-layout.jsx
+++ client/src/components/chat/chat-layout.jsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "@/components/chat/sidebar";
+import MobileBottomNav from "@/components/chat/mobile-bottom-nav";
 
 const ChatLayout = () => {
   return (
-    <div className="flex h-screen gap-1 px-2 py-1">
-      <Sidebar />
-      <div className="flex flex-1 gap-1">
+    <div className="flex h-screen flex-col lg:flex-row gap-1 px-2 py-1 pb-16 lg:pb-1 relative">
+      <div className="hidden lg:flex">
+        <Sidebar />
+      </div>
+      <div className="flex flex-1 gap-1 overflow-hidden">
         <Outlet />
       </div>
+      <MobileBottomNav />
     </div>
   );
 };
