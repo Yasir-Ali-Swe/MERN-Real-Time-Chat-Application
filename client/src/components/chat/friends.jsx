89bad8379ede5c5e5a0486b3e954d0901b6648bc
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-24 02:00:46 +0500

    Update friends component

--- client/src/components/chat/friends.jsx
+++ client/src/components/chat/friends.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-const Friends = () => {
+const Friends = ({ id, name }) => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-3xl font-semibold underline">Friends</h1>
-    </div>
+    <Link
+      to={`/friends/${id}`}
+      className="py-2 border-b hover:bg-accent cursor-pointer block"
+    >
+      <div className="flex items-center gap-2 px-2">
+        <Avatar className="w-8 h-8">
+          <AvatarImage src={`https://i.pravatar.cc/150?u=${id || "user"}`} />
+          <AvatarFallback>{name.charAt(0)}</AvatarFallback>
+        </Avatar>
+        <span className="truncate">{name}</span>
+      </div>
+    </Link>
   );
 };
 
