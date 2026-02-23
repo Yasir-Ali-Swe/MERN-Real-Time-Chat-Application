ba8abc7f50fa844ec8ba3065c75fafe8a2b351a3
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-23 22:55:47 +0500

    Update protected routes

--- client/src/routes/protected-routes.jsx
+++ client/src/routes/protected-routes.jsx
@@ -1,6 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Loader } from "lucide-react";
+import Sidebar from "@/components/chat/sidebar";
 
 const ProtectedRoutes = () => {
   const { isAuthenticated, isLoading } = useSelector((state) => state.auth);
@@ -17,7 +18,21 @@ const ProtectedRoutes = () => {
     return <Navigate to="/auth/login" replace />;
   }
 
-  return <Outlet />;
+  return (
+    <div className="flex h-[calc(100vh-12px)] gap-1 px-2 py-1">
+      {/* Sidebar */}
+      <Sidebar />
+      {/* Conversation list */}
+      <div className="w-72 border-2 flex items-center justify-center">
+        <h1 className="text-xl font-semibold">Conversation</h1>
+      </div>
+
+      {/* Chat / Outlet */}
+      <div className="flex-1 border-2 flex items-center justify-center">
+        <Outlet />
+      </div>
+    </div>
+  );
 };
 
 export default ProtectedRoutes;
