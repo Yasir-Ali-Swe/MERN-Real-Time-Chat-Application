62fe9fe13407771af28d9358821e47cf0f91c715
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-24 01:45:29 +0500

    Update friends list component

--- client/src/components/chat/friends-list.jsx
+++ client/src/components/chat/friends-list.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const FriendsList = () => {
   return (
-    <div className="w-72">
+    <div className="h-full w-full">
       <h1 className="text-xl font-semibold m-2">Friends</h1>
-      <h1 className="text-center text-muted-foreground">No friends found.</h1>
+      <div className="flex flex-col gap-2 p-2">
+        <Link
+          to="/friends/3"
+          className="p-3 border rounded-md hover:bg-accent cursor-pointer block"
+        >
+          Friend 1
+        </Link>
+        <h1 className="text-center text-muted-foreground mt-4">
+          No other friends found.
+        </h1>
+      </div>
     </div>
   );
 };
