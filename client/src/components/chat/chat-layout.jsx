3df6dab8403083689a9b7c28cb6726681007b083
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-24 02:44:32 +0500

    Chat Layout

--- client/src/components/chat/chat-layout.jsx
+++ client/src/components/chat/chat-layout.jsx
@@ -4,7 +4,7 @@ import MobileBottomNav from "@/components/chat/mobile-bottom-nav";
 
 const ChatLayout = () => {
   return (
-    <div className="flex h-screen flex-col lg:flex-row gap-1 px-2 py-1 pb-16 lg:pb-1 relative">
+    <div className="flex h-screen flex-col lg:flex-row gap-1 p-1  pb-16 lg:pb-1 relative">
       <div className="hidden lg:flex">
         <Sidebar />
       </div>
