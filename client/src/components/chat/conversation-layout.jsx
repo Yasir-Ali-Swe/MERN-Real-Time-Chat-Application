105d56265df6d8ff5a15781875051d744eb8ce62
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-24 01:45:24 +0500

    Update conversation layout component

--- client/src/components/chat/conversation-layout.jsx
+++ client/src/components/chat/conversation-layout.jsx
@@ -1,14 +1,20 @@
-import { Outlet } from "react-router-dom";
-import ConversationPlaceholder from "@/components/chat/conversation-placeholder";
+import { Outlet, useParams } from "react-router-dom";
 
-const ConversationLayout = () => {
+const ConversationLayout = ({ leftPanel }) => {
+  const { id } = useParams();
+  const isChatActive = !!id;
+
   return (
     <>
-      <div className="w-72 border">
-        <Outlet />
+      <div
+        className={`w-full lg:w-72 border h-full overflow-hidden ${isChatActive ? "hidden lg:block" : "block"}`}
+      >
+        {leftPanel}
       </div>
-      <div className="flex flex-1">
-        <ConversationPlaceholder />
+      <div
+        className={`flex-1 h-full ${isChatActive ? "flex" : "hidden lg:flex"}`}
+      >
+        <Outlet />
       </div>
     </>
   );
