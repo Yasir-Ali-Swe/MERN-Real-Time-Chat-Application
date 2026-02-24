eefb7edf77929188b436183a0d5e6fc14fde11eb
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-24 15:46:21 +0500

    Add chat API helper

---
+++ client/src/lib/chat-api.js
@@ -0,0 +1,26 @@
+import api from "./axios";
+
+export const getUsers = async () => {
+    const response = await api.get("/users");
+    return response.data;
+};
+
+export const getUserConversations = async () => {
+    const response = await api.get("/messages/get-user-conversations");
+    return response.data;
+};
+
+export const getConversationMessages = async (conversationId) => {
+    const response = await api.get(`/messages/get-conversation/${conversationId}`);
+    return response.data;
+};
+
+export const sendMessageApi = async (data) => {
+    const response = await api.post("/messages/send-message", data);
+    return response.data;
+};
+
+export const markAsReadApi = async (conversationId) => {
+    const response = await api.patch(`/messages/mark-as-read/${conversationId}`);
+    return response.data;
+};
