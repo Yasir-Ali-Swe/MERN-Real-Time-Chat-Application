b42deb08a1c0cfe6f8dd16d519bd6c9ff032991e
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-24 17:19:12 +0500

    Update user routes

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getUsers } from "../controllers/user.controller.js";
+import { getUsers, updateProfile } from "../controllers/user.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = express.Router();
 
 router.get("/", authMiddleware, getUsers);
+router.patch("/me", authMiddleware, upload.single("avatar"), updateProfile);
 
 export default router;
