7b2b596bd147754bae05e2d45066bcf0f3f0e419
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-24 15:46:46 +0500

    Update message routes

--- server/routes/message.routes.js
+++ server/routes/message.routes.js
@@ -2,6 +2,8 @@ import express from "express";
 import {
   sendMessage,
   getConversation,
+  getUserConversations,
+  markAsRead,
 } from "../controllers/message.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 
@@ -13,4 +15,7 @@ router.get(
   authMiddleware,
   getConversation,
 );
+router.get("/get-user-conversations", authMiddleware, getUserConversations);
+router.patch("/mark-as-read/:conversationId", authMiddleware, markAsRead);
+
 export default router;
