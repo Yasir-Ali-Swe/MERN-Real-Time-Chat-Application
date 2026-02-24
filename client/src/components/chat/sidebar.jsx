2fe05d937b910b65645ee85e6feb0cbc10f2175f
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-24 17:18:50 +0500

    Update chat sidebar component

--- client/src/components/chat/sidebar.jsx
+++ client/src/components/chat/sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Tooltip,
   TooltipTrigger,
@@ -8,24 +8,111 @@ import { Card } from "@/components/ui/card";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
   DropdownMenu,
   DropdownMenuTrigger,
   DropdownMenuContent,
   DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
-import { SquareUser, MessageCircle, MessageCircleMore } from "lucide-react";
+import {
+  SquareUser,
+  MessageCircle,
+  MessageCircleMore,
+  UserPen,
+  Mail,
+  User,
+  ImagePlus,
+} from "lucide-react";
 import { ModeToggle } from "@/components/toggle-theme";
 import { logout } from "@/lib/auth-api";
+import { updateProfileApi } from "@/lib/chat-api";
 import { useMutation } from "@tanstack/react-query";
-import { useDispatch } from "react-redux";
-import { logout as logoutAction } from "@/features/auth/auth-slice";
+import { useDispatch, useSelector } from "react-redux";
+import { logout as logoutAction, setUser } from "@/features/auth/auth-slice";
 import { toast } from "react-hot-toast";
+import FullScreenLoader from "@/components/ui/full-screen-loader";
+import { createFormData } from "@/lib/file-utils";
 
 const Sidebar = () => {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    avatar: null,
+  });
+  const [preview, setPreview] = useState(null);
+
   const navigate = useNavigate();
   const location = useLocation();
   const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (user) {
+      setFormData({
+        fullName: user.fullName || "",
+        email: user.email || "",
+        avatar: null,
+      });
+      setPreview(null);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    return () => {
+      if (preview) URL.revokeObjectURL(preview);
+    };
+  }, [preview]);
+
+  const { mutate: updateProfile, isPending: isUpdating } = useMutation({
+    mutationFn: updateProfileApi,
+    onSuccess: (data) => {
+      toast.success(data.message || "Profile updated successfully");
+      dispatch(setUser(data.user));
+    },
+    onError: (error) => {
+      toast.error(error.response?.data?.message || "Failed to update profile");
+    },
+  });
+
+  const handleAvatarClick = () => {
+    document.getElementById("avatarInput").click();
+  };
+  const handleChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleAvatarChange = (e) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    setFormData((prev) => ({ ...prev, avatar: file }));
+    setPreview(URL.createObjectURL(file));
+  };
+
+  const handleUpdate = () => {
+    // Basic email validation
+    if (formData.email && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) {
+      return toast.error("Invalid email format");
+    }
+
+    const fd = createFormData(formData);
+    updateProfile(fd);
+  };
 
   const { mutate: logoutUser } = useMutation({
     mutationFn: logout,
@@ -66,45 +153,139 @@ const Sidebar = () => {
   );
 
   return (
-    <Card className="flex h-full w-14 flex-col items-center justify-between rounded-none bg-background py-2 px-3">
-      <div className="flex flex-col items-center gap-4">
-        {renderTooltipButton(
-          <MessageCircleMore className="size-5" />,
-          "NexTalk",
-          "/conversations",
-          "ghost",
-        )}
-        <div className="flex flex-col items-center gap-2">
+    <>
+      {isUpdating && <FullScreenLoader message="Updating profile..." />}
+      <Card className="flex h-full w-14 flex-col items-center justify-between rounded-none bg-background py-2 px-3">
+        <div className="flex flex-col items-center gap-4">
           {renderTooltipButton(
-            <MessageCircle className="size-4" />,
-            "Conversations",
+            <MessageCircleMore className="size-5" />,
+            "NexTalk",
             "/conversations",
-            getVariant("/conversations"),
+            "ghost",
           )}
+          <div className="flex flex-col items-center gap-2">
+            {renderTooltipButton(
+              <MessageCircle className="size-4" />,
+              "Conversations",
+              "/conversations",
+              getVariant("/conversations"),
+            )}
 
-          {renderTooltipButton(
-            <SquareUser className="size-4" />,
-            "Friends",
-            "/friends",
-            getVariant("/friends"),
-          )}
+            {renderTooltipButton(
+              <SquareUser className="size-4" />,
+              "Friends",
+              "/friends",
+              getVariant("/friends"),
+            )}
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col items-center gap-3">
-        <ModeToggle />
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Avatar className="h-8 w-8 cursor-pointer">
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>JD</AvatarFallback>
-            </Avatar>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent side="right" align="end">
-            <DropdownMenuItem onClick={logoutUser}>Logout</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
-    </Card>
+        <div className="flex flex-col items-center gap-3">
+          <Dialog>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <DialogTrigger asChild>
+                  <Button size="icon" variant="default">
+                    <UserPen className="size-4" />
+                  </Button>
+                </DialogTrigger>
+              </TooltipTrigger>
+              <TooltipContent side="right">Edit profile</TooltipContent>
+            </Tooltip>
+
+            <DialogContent className="sm:max-w-md">
+              <DialogHeader>
+                <DialogTitle>Edit profile</DialogTitle>
+                <DialogDescription>
+                  Update your personal information here.
+                </DialogDescription>
+              </DialogHeader>
+
+              <div className="space-y-4">
+                {/* Avatar */}
+                <div className="flex items-center gap-4">
+                  <Avatar
+                    className="size-24 mx-auto cursor-pointer"
+                    onClick={() => !isUpdating && handleAvatarClick()}
+                  >
+                    {preview || user?.profilePicture ? (
+                      <AvatarImage src={preview || user?.profilePicture} />
+                    ) : (
+                      <AvatarFallback>
+                        <ImagePlus className="size-5 text-primary" />
+                      </AvatarFallback>
+                    )}
+                  </Avatar>
+                  <input
+                    id="avatarInput"
+                    type="file"
+                    accept="image/jpeg, image/png, image/webp"
+                    className="hidden"
+                    onChange={handleAvatarChange}
+                    disabled={isUpdating}
+                  />
+                </div>
+
+                {/* Full name */}
+                <div className="space-y-1 relative">
+                  <Label htmlFor="fullName">Full name</Label>
+                  <Input
+                    id="fullName"
+                    name="fullName"
+                    placeholder="Enter your full name"
+                    value={formData.fullName}
+                    onChange={handleChange}
+                    disabled={isUpdating}
+                  />
+                  <div className="absolute right-3 top-6.5">
+                    <User className="size-5 text-primary" />
+                  </div>
+                </div>
+
+                {/* Email */}
+                <div className="space-y-1 relative">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder="Enter your email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    disabled={isUpdating}
+                  />
+                  <div className="absolute right-3 top-6.5">
+                    <Mail className="size-5 text-primary" />
+                  </div>
+                </div>
+
+                {/* Actions */}
+                <Button
+                  onClick={handleUpdate}
+                  className="w-full mt-2"
+                  disabled={isUpdating}
+                >
+                  {isUpdating ? "Updating..." : "Update"}
+                </Button>
+              </div>
+            </DialogContent>
+          </Dialog>
+          <ModeToggle />
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Avatar className="h-8 w-8 cursor-pointer border">
+                <AvatarImage src={user?.profilePicture} />
+                <AvatarFallback>
+                  {user?.fullName?.charAt(0)?.toUpperCase() || "U"}
+                </AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent side="right" align="end">
+              <DropdownMenuItem onClick={logoutUser}>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </Card>
+    </>
   );
 };
 
