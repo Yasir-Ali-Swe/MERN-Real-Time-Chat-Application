c7bb6ca46327e50830848992f0ba4fe0896be277
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-02-24 15:46:26 +0500

    Update database connection config

--- server/config/db.connect.js
+++ server/config/db.connect.js
@@ -5,7 +5,7 @@ import { MONGO_URI } from "./env.js";
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI);
-    console.log("MongoDB connected successfully");
+    console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
     process.exit(1);
